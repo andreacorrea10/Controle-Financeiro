b8db997aeb44748a02428ab0f2f235e1cc0178cc
andreacorrea10 <99226514+andreacorrea10@users.noreply.github.com>
2022-03-21 23:57:54 -0300

    Initial commit

---
+++ src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from '../types/Item';
+
+export const items: Item[] = [
+    {date: new Date(2022, 2, 18), category: 'food', title: 'McDonalds', value: 35.12},
+    {date: new Date(2022, 2, 19), category: 'food', title: 'BurgerKing', value: 28.02},
+    {date: new Date(2022, 2, 20), category: 'rent', title: 'Aluguel Apt', value: 3500},
+    {date: new Date(2022, 3, 7), category: 'salary', title: 'InovaTech', value: 8900},
+];
